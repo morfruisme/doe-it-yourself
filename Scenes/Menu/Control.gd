9b5bc2db6cbe590f16fac0caea81e2179b644f90
LE GRAND TEFA <fate.apangue-tsangou@imt-atlantique.net>
2025-01-30 10:07:48 +0100

    tem

---
+++ Scenes/Menu/Control.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _ready():
+	$Doe/AnimationPlayer.play("starting")
+	$VBoxContainer/Start.connect("pressed", self, "_on_StartButton_pressed")
+	$VBoxContainer2/QUIT.connect("pressed", self, "_on_QuitButton_pressed")
+
+func _on_StartButton_pressed():
+	# Charger et démarrer la scène du jeu
+	get_tree().change_scene("res://Scenes/Player/scene.tscn")
+
+func _on_QuitButton_pressed():
+	# Quitter le jeu
+	get_tree().quit()
